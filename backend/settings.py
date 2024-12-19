0b7394f91e57f0cf90508a4ebef9b3ca0e810020
xaviercallens <callensxavier@gmail.com>
2024-12-19 09:41:29 +0100

    Update settings.py to fix GEM AI

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "GEM AI"
+    title: str = "GEMAI"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Start chatting"
